e33664532cf60b0d06da1b33b92c40e771f637a0
Anton <laant@mail.ru>
2023-10-10 21:01:02 +0500

    Добавьте файлы проекта.

---
+++ MyTelegramChatGPT/ChatGPT.cs
@@ -0,0 +1,32 @@
+﻿using OpenAI.ChatGpt;
+using OpenAI.ChatGpt.Models.ChatCompletion.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace MyTelegramChatGPT
+{
+    internal class ChatGPT
+    {
+        public async Task<string> Ask(string text)
+        {
+            string nameKey = "Key_API";
+            string? key = Environment.GetEnvironmentVariable(nameKey);
+            if(key==null)
+            {
+                throw new InvalidOperationException($"Переменная окружения {nameKey} не задана");
+            }
+            using var client = new OpenAiClient(key);
+            
+            string response = await client.GetChatCompletions(new UserMessage(text), maxTokens: 300);
+            
+            return response;
+        }
+
+    }
+
+}
---
+++ MyTelegramChatGPT/Program.cs
@@ -0,0 +1,84 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Telegram.Bot.Types;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Exceptions;
+using MyTelegramChatGPT;
+
+string name_Token = "Token_TelegramBot";
+string? token = Environment.GetEnvironmentVariable(name_Token);
+if (token == null)
+{
+    throw new InvalidOperationException($"Переменная среды окружения {name_Token} не задана ");
+}
+
+var botClient = new TelegramBotClient(token);
+var chatGPTClient = new ChatGPT(); // на основе нашего класса
+
+using CancellationTokenSource cts = new();
+
+
+ReceiverOptions receiverOptions = new()
+{
+    AllowedUpdates = Array.Empty<UpdateType>() // receive all update types except ChatMember related updates
+};
+
+botClient.StartReceiving(
+    updateHandler: HandleUpdateAsync,
+    pollingErrorHandler: HandlePollingErrorAsync,
+    receiverOptions: receiverOptions,
+    cancellationToken: cts.Token
+);
+
+var me = await botClient.GetMeAsync();
+
+Console.WriteLine($"Start listening for @{me.Username}");
+Console.ReadLine();
+
+// Send cancellation request to stop bot
+cts.Cancel();
+
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+    // Only process Message updates: https://core.telegram.org/bots/api#message
+    if (update.Message is not { } message)
+        return;
+    // Only process text messages
+    if (message.Text is not { } messageText)
+        return;
+
+    var chatId = message.Chat.Id;
+
+    Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
+
+    var answer = await chatGPTClient.Ask(messageText);
+
+    // Echo received message text
+    Message sentMessage = await botClient.SendTextMessageAsync(
+        chatId: chatId,
+        text: answer,
+        cancellationToken: cancellationToken);
+}
+
+Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+    var ErrorMessage = exception switch
+    {
+        ApiRequestException apiRequestException
+            => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+        _ => exception.ToString()
+    };
+
+    Console.WriteLine(ErrorMessage);
+    return Task.CompletedTask;
+}
+
+
+
+
+
+
+
+
+
